b9354993af8180e2204f9ab10fe2c0ac6353aba3
hhko <hyungho.ko@gmail.com>
2026-01-31 13:02:58 +0900

    refactor(tutorials): Cqrs05/Cqrs06 튜토리얼 프로젝트 이름 변경
    - Cqrs05Services -> Cqrs06Services
    - Cqrs06EndpointLayered -> Cqrs05EndpointLayered
    
    순서에 맞게 튜토리얼 프로젝트명 재정렬

--- Tutorials/Cqrs06EndpointLayered/Src/Cqrs06EndpointLayered.Applications/AssemblyReference.cs
+++
@@ -1,8 +0,0 @@
-using System.Reflection;
-
-namespace Cqrs06EndpointLayered.Applications;
-
-public static class AssemblyReference
-{
-    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
-}
